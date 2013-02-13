27f6dc93a428c241d97786a3118099a17c3e6196
Tom Everett <tome@criticalmass.com>
2013-02-13 16:17:36 -0700

    admin
    Signed-off-by: Tom Everett <tome@criticalmass.com>

---
+++ pragmatach-adminapp/src/main/java/com/khubla/pragmatach/plugin/adminapp/AdminController.java
@@ -0,0 +1,9 @@
+package com.khubla.pragmatach.plugin.adminapp;
+
+import com.khubla.pragmatach.plugin.freemarker.FreemarkerController;
+
+/**
+ * @author tome
+ */
+public class AdminController extends FreemarkerController {
+}
