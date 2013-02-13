e3ba5f75c079cc20551d359dce54f77f709036e6
Tom Everett <tome@criticalmass.com>
2013-02-13 16:25:10 -0700

    admin stuff
    Signed-off-by: Tom Everett <tome@criticalmass.com>

--- pragmatach-adminapp/src/main/java/com/khubla/pragmatach/plugin/adminapp/AdminController.java
+++ pragmatach-adminapp/src/main/java/com/khubla/pragmatach/plugin/adminapp/AdminController.java
@@ -1,9 +1,21 @@
 package com.khubla.pragmatach.plugin.adminapp;
 
+import com.khubla.pragmatach.framework.annotation.Controller;
+import com.khubla.pragmatach.framework.annotation.Route;
+import com.khubla.pragmatach.framework.api.PragmatachException;
+import com.khubla.pragmatach.framework.api.Request;
+import com.khubla.pragmatach.framework.api.Response;
 import com.khubla.pragmatach.plugin.freemarker.FreemarkerController;
+import com.khubla.pragmatach.plugin.freemarker.FreemarkerTemplate;
 
 /**
  * @author tome
  */
+@Controller
+@FreemarkerTemplate(template = "admin.ftl")
 public class AdminController extends FreemarkerController {
+   @Route(uri = "/")
+   public Response render(Request request) throws PragmatachException {
+      return super.render(request);
+   }
 }
