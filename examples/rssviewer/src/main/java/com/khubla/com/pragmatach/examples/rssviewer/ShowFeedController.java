ee0e706a2578569da4ce562d286bc43d7d674639
Tom Everett <tome@criticalmass.com>
2013-02-16 13:12:48 -0700

    rssview basically works
    Signed-off-by: Tom Everett <tome@criticalmass.com>

--- examples/rssviewer/src/main/java/com/khubla/com/pragmatach/examples/rssviewer/ShowFeedController.java
+++ examples/rssviewer/src/main/java/com/khubla/com/pragmatach/examples/rssviewer/ShowFeedController.java
@@ -1,5 +1,8 @@
 package com.khubla.com.pragmatach.examples.rssviewer;
 
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 import com.khubla.pragmatach.framework.annotation.Controller;
 import com.khubla.pragmatach.framework.annotation.Route;
 import com.khubla.pragmatach.framework.annotation.Route.HttpMethod;
@@ -8,39 +11,67 @@ import com.khubla.pragmatach.framework.api.Response;
 import com.khubla.pragmatach.framework.api.form.Form;
 import com.khubla.pragmatach.plugin.freemarker.FreemarkerController;
 import com.khubla.pragmatach.plugin.freemarker.FreemarkerTemplate;
+import com.sun.syndication.feed.synd.SyndFeed;
+import com.sun.syndication.io.SyndFeedInput;
+import com.sun.syndication.io.XmlReader;
 
 @Controller
 @FreemarkerTemplate(template = "showfeed.html")
 public class ShowFeedController extends FreemarkerController {
-   @Route(uri = "/showfeed", method = HttpMethod.post)
-   public Response showFeed() throws PragmatachException {
-      /*
-       * get the posted form
-       */
-      Form form = this.getRequest().getFormData();
-      if (null != form) {
-         /*
-          * get the url
-          */
-         feedURL = form.getFormItemValue("url");
-         System.out.println(feedURL);
-      }
-      /*
-       * render
-       */
-      return super.render();
-   }
+   /**
+    * the URL
+    */
+   private String feedURL;
+   /**
+    * the feed
+    */
+   private SyndFeed syndFeed;
 
    public String getFeedURL() {
       return feedURL;
+   }
+
+   public SyndFeed getSyndFeed() {
+      return syndFeed;
    }
 
    public void setFeedURL(String feedURL) {
       this.feedURL = feedURL;
    }
 
-   /**
-    * the URL
-    */
-   private String feedURL;
+   public void setSyndFeed(SyndFeed syndFeed) {
+      this.syndFeed = syndFeed;
+   }
+
+   @Route(uri = "/showfeed", method = HttpMethod.post)
+   public Response showFeed() throws PragmatachException {
+      try {
+         /*
+          * get the posted form
+          */
+         final Form form = getRequest().getFormData();
+         if (null != form) {
+            /*
+             * get the url
+             */
+            feedURL = form.getFormItemValue("url");
+            /*
+             * get feed
+             */
+            final URL url = new URL(feedURL);
+            final HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+            /*
+             * parse it
+             */
+            final SyndFeedInput input = new SyndFeedInput();
+            syndFeed = input.build(new XmlReader(httpURLConnection));
+         }
+         /*
+          * render
+          */
+         return super.render();
+      } catch (final Exception e) {
+         throw new PragmatachException("Exception in showFeed", e);
+      }
+   }
 }
