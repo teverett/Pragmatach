8eae8356fa281562fab34ef4a9ba310e7c44c6b2
Tom Everett <tome@criticalmass.com>
2013-02-17 15:38:39 -0700

    oops
    Signed-off-by: Tom Everett <tome@criticalmass.com>

--- generate_example_project.sh
+++ generate_example_project.sh
@@ -1,2 +1,2 @@
-java -jar pragmatach-console/target/pragmatach-console-1.0-SNAPSHOT-jar-with-dependencies.jar --operation=new --name=exampleproject --namespace=com.khubla.com.pragmatach.exampleproject
+java -jar pragmatach-console/target/pragmatach-console-1.0-SNAPSHOT-jar-with-dependencies.jar --operation=new --name=exampleproject --namespace=com.khubla.pragmatach.exampleproject
  
