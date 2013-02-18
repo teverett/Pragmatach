515298a109bd00b065d790c5426d141127172c2b
Tom Everett <tome@criticalmass.com>
2013-02-18 15:28:00 -0700

    better routes display
    Signed-off-by: Tom Everett <tome@criticalmass.com>

--- pragmatach-adminapp/src/main/java/com/khubla/pragmatach/plugin/adminapp/ShowRoutesController.java
+++ pragmatach-adminapp/src/main/java/com/khubla/pragmatach/plugin/adminapp/ShowRoutesController.java
@@ -28,6 +28,22 @@ public class ShowRoutesController extends FreemarkerController {
       return POSTRoutes;
    }
 
+   public String getRouteParameters(PragmatachRoute pragmatachRoute) {
+      String ret = "";
+      int i = 0;
+      final Class<?>[] types = pragmatachRoute.getMethod().getParameterTypes();
+      if (null != types) {
+         for (final Class<?> type : types) {
+            if (i != 0) {
+               ret += ",";
+            }
+            i++;
+            ret += type.getName();
+         }
+      }
+      return ret;
+   }
+
    @Route(uri = "/pragmatach/admin/routes")
    public Response render() throws PragmatachException {
       POSTRoutes = PragmatachRoutes.getInstance().getPOSTRoutes();
