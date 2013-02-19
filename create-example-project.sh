aa694f3a64f7df6bd605a5cd054783cd6706692e
Tom Everett <tome@criticalmass.com>
2013-02-18 18:23:07 -0700

    version 1.0.0
    Signed-off-by: Tom Everett <tome@criticalmass.com>

--- create-example-project.sh
+++ create-example-project.sh
@@ -1,6 +1,6 @@
 mvn archetype:generate                                  \
   -DarchetypeGroupId=com.khubla.pragmatach                \
   -DarchetypeArtifactId=pragmatach-archetype          \
-  -DarchetypeVersion=1.0-SNAPSHOT                \
+  -DarchetypeVersion=1.0.0                \
   -DgroupId=com.khubla.exampleproject                               \
   -DartifactId=myexampleproject
