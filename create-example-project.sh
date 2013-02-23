ffa37b17a6ef888617f969aeba282667a1f9cdd1
Tom Everett <tome@criticalmass.com>
2013-02-23 11:50:39 -0700

    update version numbers to 1.0.4
    Signed-off-by: Tom Everett <tome@criticalmass.com>

--- create-example-project.sh
+++ create-example-project.sh
@@ -1,6 +1,6 @@
 mvn archetype:generate                                  \
   -DarchetypeGroupId=com.khubla.pragmatach                \
   -DarchetypeArtifactId=pragmatach-archetype          \
-  -DarchetypeVersion=1.0.3                \
+  -DarchetypeVersion=1.0.4                \
   -DgroupId=com.khubla.exampleproject                               \
   -DartifactId=myexampleproject
