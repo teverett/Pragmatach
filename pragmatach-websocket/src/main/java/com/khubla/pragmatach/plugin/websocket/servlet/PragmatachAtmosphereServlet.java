4c9dabc3b803c235e25f3ffb49fe5dc839c0bc85
Tom Everett <tome@criticalmass.com>
2013-02-24 19:47:50 -0700

    websockets via atmosphere
    Signed-off-by: Tom Everett <tome@criticalmass.com>

---
+++ pragmatach-websocket/src/main/java/com/khubla/pragmatach/plugin/websocket/servlet/PragmatachAtmosphereServlet.java
@@ -0,0 +1,13 @@
+package com.khubla.pragmatach.plugin.websocket.servlet;
+
+import org.atmosphere.cpr.AtmosphereServlet;
+
+/**
+ * @author tome
+ */
+public class PragmatachAtmosphereServlet extends AtmosphereServlet {
+   /**
+    * 
+    */
+   private static final long serialVersionUID = 1L;
+}
