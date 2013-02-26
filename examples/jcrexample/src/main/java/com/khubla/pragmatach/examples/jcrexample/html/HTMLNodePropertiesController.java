c349cab15a08f587d2dad9970354c2e9ff6f981d
Tom Everett <tome@criticalmass.com>
2013-02-25 18:04:02 -0700

    get the nodes
    Signed-off-by: Tom Everett <tome@criticalmass.com>

--- examples/jcrexample/src/main/java/com/khubla/pragmatach/examples/jcrexample/html/HTMLNodePropertiesController.java
+++ examples/jcrexample/src/main/java/com/khubla/pragmatach/examples/jcrexample/html/HTMLNodePropertiesController.java
@@ -1,5 +1,8 @@
 package com.khubla.pragmatach.examples.jcrexample.html;
 
+import javax.jcr.Node;
+import javax.jcr.Session;
+
 import com.khubla.pragmatach.framework.annotation.Controller;
 import com.khubla.pragmatach.framework.annotation.Route;
 import com.khubla.pragmatach.framework.annotation.RouteParameter;
@@ -19,14 +22,38 @@ public class HTMLNodePropertiesController extends FreemarkerController {
     * JCRSessionFactory
     */
    protected final JCRSessionFactory jcrSessionFactory = new JCRSessionFactory();
+   /**
+    * the node
+    */
+   private Node node;
+
+   public Node getNode() {
+      return node;
+   }
 
    @Route(uri = "example/html/properties/")
    public Response render() throws PragmatachException {
-      return super.render();
+      try {
+         final Session session = jcrSessionFactory.getSession();
+         node = session.getRootNode();
+         return super.render();
+      } catch (final Exception e) {
+         throw new PragmatachException("Exception in render", e);
+      }
    }
 
    @Route(uri = "example/html/properties/@nodeName")
    public Response render(@RouteParameter(name = "nodeName") String nodeName) throws PragmatachException {
-      return super.render();
+      try {
+         final Session session = jcrSessionFactory.getSession();
+         node = session.getRootNode().getNode(nodeName);
+         return super.render();
+      } catch (final Exception e) {
+         throw new PragmatachException("Exception in render", e);
+      }
+   }
+
+   public void setNode(Node node) {
+      this.node = node;
    }
 }
