5de829e55b05ed210a051e5bdccf933f593d7876
Tom Everett <tome@criticalmass.com>
2013-03-02 20:02:44 -0700

    update poms to 1.0.6
    Signed-off-by: Tom Everett <tome@criticalmass.com>

--- create-example-project.sh
+++ create-example-project.sh
@@ -1,6 +1,6 @@
 mvn archetype:generate                                  \
   -DarchetypeGroupId=com.khubla.pragmatach                \
   -DarchetypeArtifactId=pragmatach-archetype          \
-  -DarchetypeVersion=1.0.5                \
+  -DarchetypeVersion=1.0.6                \
   -DgroupId=com.khubla.exampleproject                               \
   -DartifactId=myexampleproject
