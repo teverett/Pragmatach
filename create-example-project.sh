73b8c0d257d28e82dd3d508258af44a6a2b04f11
Tom Everett <tome@criticalmass.com>
2013-03-03 09:42:05 -0700

    update pom version to 1.0.8
    Signed-off-by: Tom Everett <tome@criticalmass.com>

--- create-example-project.sh
+++ create-example-project.sh
@@ -1,6 +1,6 @@
 mvn archetype:generate                                  \
   -DarchetypeGroupId=com.khubla.pragmatach                \
   -DarchetypeArtifactId=pragmatach-archetype          \
-  -DarchetypeVersion=1.0.7                \
+  -DarchetypeVersion=1.0.8                \
   -DgroupId=com.khubla.exampleproject                               \
   -DartifactId=myexampleproject
