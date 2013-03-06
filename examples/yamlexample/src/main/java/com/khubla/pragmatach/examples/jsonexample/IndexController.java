08995524773f0f8cec30473c9bd4d183bd7abcb3
Tom Everett <tome@criticalmass.com>
2013-03-06 12:40:30 -0700

    yaml example
    Signed-off-by: Tom Everett <tome@criticalmass.com>

---
+++ examples/yamlexample/src/main/java/com/khubla/pragmatach/examples/jsonexample/IndexController.java
@@ -0,0 +1,39 @@
+package com.khubla.pragmatach.examples.jsonexample;
+
+import java.util.Date;
+
+import com.khubla.pragmatach.framework.annotation.Controller;
+import com.khubla.pragmatach.framework.annotation.Route;
+import com.khubla.pragmatach.framework.api.PragmatachException;
+import com.khubla.pragmatach.framework.api.Response;
+import com.khubla.pragmatach.plugin.yaml.YAMLController;
+
+@Controller(name = "IndexController")
+public class IndexController extends YAMLController {
+   /**
+    * the message
+    */
+   private final String message = "hello world";
+   /**
+    * the time
+    */
+   private String time;
+
+   public String getMessage() {
+      return message;
+   }
+
+   public String getTime() {
+      return time;
+   }
+
+   @Route(uri = "/")
+   public Response render() throws PragmatachException {
+      time = new Date().toString();
+      return super.render();
+   }
+
+   public void setTime(String time) {
+      this.time = time;
+   }
+}
