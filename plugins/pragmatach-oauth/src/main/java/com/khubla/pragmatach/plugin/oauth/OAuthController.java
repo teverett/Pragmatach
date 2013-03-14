ca033306734702e016c648261be06fa690844628
Tom Everett <tome@criticalmass.com>
2013-03-14 16:40:55 -0600

    remove oauth
    Signed-off-by: Tom Everett <tome@criticalmass.com>

--- plugins/pragmatach-oauth/src/main/java/com/khubla/pragmatach/plugin/oauth/OAuthController.java
+++
@@ -1,9 +0,0 @@
-package com.khubla.pragmatach.plugin.oauth;
-
-import com.khubla.pragmatach.plugin.freemarker.FreemarkerController;
-
-/**
- * @author tome
- */
-public class OAuthController extends FreemarkerController {
-}
