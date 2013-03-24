93fb4e2efbae9a4ad6d86498e3817d8df37494e1
Tom Everett <tome@criticalmass.com>
2013-03-24 15:53:27 -0600

    styled the javadoc
    Signed-off-by: Tom Everett <tome@criticalmass.com>

--- makesite.sh
+++ makesite.sh
@@ -3,9 +3,7 @@ cd site
 mvn clean package
 cp -R target/site/* ../../pragmatach-site/
 cd ..
-cd pragmatach-framework
 mvn javadoc:javadoc
-cp -R target/site/apidocs ../../pragmatach-site/
-cd ..
+cp -R target/site/apidocs ../pragmatach-site/
 mvn clean package deploy
 cp -R repo ../pragmatach-site/
