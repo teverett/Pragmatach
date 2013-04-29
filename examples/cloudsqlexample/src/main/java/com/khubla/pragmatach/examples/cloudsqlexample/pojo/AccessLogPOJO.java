0c1fc575b4587d516989db18e91fc3749b2d3a62
Tom Everett <tome@criticalmass.com>
2013-04-28 20:18:51 -0600

    cloudsql games
    Signed-off-by: Tom Everett <tome@criticalmass.com>

---
+++ examples/cloudsqlexample/src/main/java/com/khubla/pragmatach/examples/cloudsqlexample/pojo/AccessLogPOJO.java
@@ -0,0 +1,58 @@
+package com.khubla.pragmatach.examples.cloudsqlexample.pojo;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import com.khubla.pragmatach.plugin.ebean.EBeanDAO;
+
+/**
+ * @author tome
+ */
+@Entity
+public class AccessLogPOJO {
+   /**
+    * primary key
+    */
+   @Id
+   @GeneratedValue
+   private Long id;
+   /**
+    * ip
+    */
+   private String ip;
+   /**
+    * date
+    */
+   private Date date;
+   /**
+    * an EBean DAO for MyExamplePOJOs
+    */
+   public static EBeanDAO<AccessLogPOJO, Long> dao = new EBeanDAO<AccessLogPOJO, Long>(AccessLogPOJO.class, Long.class);
+
+   public Long getId() {
+      return id;
+   }
+
+   public String getIp() {
+      return ip;
+   }
+
+   public void setIp(String ip) {
+      this.ip = ip;
+   }
+
+   public Date getDate() {
+      return date;
+   }
+
+   public void setDate(Date date) {
+      this.date = date;
+   }
+
+   public void setId(Long id) {
+      this.id = id;
+   }
+}
--- examples/cloudsqlexample/src/test/java/test/com/khubla/pragmatach/examples/ebeanexample/TestMyExamplePOJO.java
+++ examples/cloudsqlexample/src/test/java/test/com/khubla/pragmatach/examples/ebeanexample/TestMyExamplePOJO.java
@@ -2,7 +2,7 @@ package test.com.khubla.pragmatach.examples.ebeanexample;
 
 import org.testng.annotations.Test;
 
-import com.khubla.pragmatach.examples.cloudsqlexample.pojo.MyExamplePOJO;
+import com.khubla.pragmatach.examples.cloudsqlexample.pojo.AccessLogPOJO;
 import com.khubla.pragmatach.framework.dao.DAO;
 import com.khubla.pragmatach.framework.test.AbstractDAOTest;
 
@@ -10,19 +10,19 @@ import com.khubla.pragmatach.framework.test.AbstractDAOTest;
  * @author tome
  */
 @Test(enabled = true)
-public class TestMyExamplePOJO extends AbstractDAOTest<MyExamplePOJO, Long> {
+public class TestMyExamplePOJO extends AbstractDAOTest<AccessLogPOJO, Long> {
    @Override
-   public DAO<MyExamplePOJO, Long> getDAO() {
-      return MyExamplePOJO.dao;
+   public DAO<AccessLogPOJO, Long> getDAO() {
+      return AccessLogPOJO.dao;
    }
 
    @Override
-   public Long getId(MyExamplePOJO myExamplePOJO) {
+   public Long getId(AccessLogPOJO myExamplePOJO) {
       return myExamplePOJO.getId();
    }
 
    @Override
-   public MyExamplePOJO getInstance() {
-      return new MyExamplePOJO();
+   public AccessLogPOJO getInstance() {
+      return new AccessLogPOJO();
    }
 }
