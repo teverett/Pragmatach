5579da0d7ed3de49bdcdf12571c81ebb6e1db222
Tom Everett <tom@khubla.com>
2014-03-25 13:26:19 -0600

    more findbugs fixes

--- plugins/pragmatach-adminapp/src/main/java/com/khubla/pragmatach/plugin/adminapp/ShowApplicationController.java
+++ plugins/pragmatach-adminapp/src/main/java/com/khubla/pragmatach/plugin/adminapp/ShowApplicationController.java
@@ -15,63 +15,81 @@ import com.khubla.pragmatach.framework.api.Response;
 @Controller(name = "pragmatachShowApplicationController")
 @View(view = "pragmatach/admin/application.html")
 public class ShowApplicationController extends SecuredAdminController {
-   /**
-    * application version from maven pom
-    */
-   private String applicationVersion;
-   /**
-    * build date from maven pom
-    */
-   private String buildDate;
+	/**
+	 * application version from maven pom
+	 */
+	private String applicationVersion;
+	/**
+	 * build date from maven pom
+	 */
+	private String buildDate;
 
-   private String findApplicationBuildDate() throws PragmatachException {
-      try {
-         final InputStream is = ShowApplicationController.class.getResourceAsStream("/version.properties");
-         if (null != is) {
-            final Properties properties = new Properties();
-            properties.load(is);
-            return properties.getProperty("maven.builddate");
-         }
-         return null;
-      } catch (final Exception e) {
-         throw new PragmatachException("Exception in findApplicationBuildDate", e);
-      }
-   }
+	private String findApplicationBuildDate() throws PragmatachException {
+		try {
+			InputStream is = null;
+			try {
+				is = ShowApplicationController.class
+						.getResourceAsStream("/version.properties");
+				if (null != is) {
+					final Properties properties = new Properties();
+					properties.load(is);
+					return properties.getProperty("maven.builddate");
+				}
+				return null;
+			} finally {
+				if (null != is) {
+					is.close();
+				}
+			}
+		} catch (final Exception e) {
+			throw new PragmatachException(
+					"Exception in findApplicationBuildDate", e);
+		}
+	}
 
-   private String findApplicationVersion() throws PragmatachException {
-      try {
-         final InputStream is = ShowApplicationController.class.getResourceAsStream("/version.properties");
-         if (null != is) {
-            final Properties properties = new Properties();
-            properties.load(is);
-            return properties.getProperty("maven.version");
-         }
-         return null;
-      } catch (final Exception e) {
-         throw new PragmatachException("Exception in findApplicationVersion", e);
-      }
-   }
+	private String findApplicationVersion() throws PragmatachException {
+		try {
+			InputStream is = null;
+			try {
+				is = ShowApplicationController.class
+						.getResourceAsStream("/version.properties");
+				if (null != is) {
+					final Properties properties = new Properties();
+					properties.load(is);
+					return properties.getProperty("maven.version");
+				}
+				return null;
+			} finally {
+				if (null != is) {
+					is.close();
+				}
+			}
+		} catch (final Exception e) {
+			throw new PragmatachException(
+					"Exception in findApplicationVersion", e);
+		}
+	}
 
-   public String getApplicationVersion() {
-      return applicationVersion;
-   }
+	public String getApplicationVersion() {
+		return applicationVersion;
+	}
 
-   public String getBuildDate() {
-      return buildDate;
-   }
+	public String getBuildDate() {
+		return buildDate;
+	}
 
-   @Route(uri = "/pragmatach/admin/application")
-   public Response render() throws PragmatachException {
-      applicationVersion = findApplicationVersion();
-      buildDate = findApplicationBuildDate();
-      return super.render();
-   }
+	@Route(uri = "/pragmatach/admin/application")
+	public Response render() throws PragmatachException {
+		applicationVersion = findApplicationVersion();
+		buildDate = findApplicationBuildDate();
+		return super.render();
+	}
 
-   public void setApplicationVersion(String applicationVersion) {
-      this.applicationVersion = applicationVersion;
-   }
+	public void setApplicationVersion(String applicationVersion) {
+		this.applicationVersion = applicationVersion;
+	}
 
-   public void setBuildDate(String buildDate) {
-      this.buildDate = buildDate;
-   }
+	public void setBuildDate(String buildDate) {
+		this.buildDate = buildDate;
+	}
 }
