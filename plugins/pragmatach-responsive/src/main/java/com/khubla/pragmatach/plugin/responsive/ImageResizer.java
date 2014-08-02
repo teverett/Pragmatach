7c25508c7505b4acd24080219acd0901ae47e921
Tom Everett <tom@khubla.com>
2014-08-02 14:54:15 -0600

    coverity bug fixes

--- plugins/pragmatach-responsive/src/main/java/com/khubla/pragmatach/plugin/responsive/ImageResizer.java
+++ plugins/pragmatach-responsive/src/main/java/com/khubla/pragmatach/plugin/responsive/ImageResizer.java
@@ -17,14 +17,16 @@ public class ImageResizer {
    public static void resize(InputStream inputImage, OutputStream outputImage, double xscale, double yscale) throws PragmatachException {
       try {
          final BufferedImage bufferedImage = ImageIO.read(inputImage);
-         final int width = (int) Math.round(bufferedImage.getWidth() * xscale);
-         final int height = (int) Math.round(bufferedImage.getHeight() * yscale);
-         final Image scaledImage = bufferedImage.getScaledInstance(width, height, Image.SCALE_SMOOTH);
-         final BufferedImage imageBuff = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-         final Graphics g = imageBuff.createGraphics();
-         g.drawImage(scaledImage, 0, 0, null, null);
-         g.dispose();
-         ImageIO.write(imageBuff, "png", outputImage);
+         if (null != bufferedImage) {
+            final int width = (int) Math.round(bufferedImage.getWidth() * xscale);
+            final int height = (int) Math.round(bufferedImage.getHeight() * yscale);
+            final Image scaledImage = bufferedImage.getScaledInstance(width, height, Image.SCALE_SMOOTH);
+            final BufferedImage imageBuff = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+            final Graphics g = imageBuff.createGraphics();
+            g.drawImage(scaledImage, 0, 0, null, null);
+            g.dispose();
+            ImageIO.write(imageBuff, "png", outputImage);
+         }
       } catch (final Exception e) {
          throw new PragmatachException("Exception in resize", e);
       }
