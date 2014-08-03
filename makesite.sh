506731c74671513bc17fbae8f1baf78bbe1a130f
Tom Everett <tom@khubla.com>
2014-08-03 11:43:20 -0600

    fix to makesite

--- makesite.sh
+++ makesite.sh
@@ -1,9 +1,11 @@
 rm -rf ../pragmatach-site/*
+mkdir ../pragmatach-site
 cd site
 mvn clean package
 cp -R target/site/* ../../pragmatach-site/
 cd ..
 mvn javadoc:javadoc
-cp -R target/site/apidocs ../pragmatach-site/
+mkdir ../pragmatach-site/apidocs
+cp -R target/site/apidocs/* ../pragmatach-site/apidocs
 mvn clean package deploy
 cp -R repo ../pragmatach-site/
