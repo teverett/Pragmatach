2442da4937e7d078104e2bc954088206107badbf
Tom Everett <tom@khubla.com>
2022-06-26 19:59:51 -0600

    gox example fix

--- examples/goxmon/src/main/java/com/khubla/goxmon/IndexController.java
+++ examples/goxmon/src/main/java/com/khubla/goxmon/IndexController.java
@@ -1,15 +1,11 @@
 package com.khubla.goxmon;
 
-import com.khubla.pragmatach.framework.annotation.Controller;
-import com.khubla.pragmatach.framework.annotation.Route;
-import com.khubla.pragmatach.framework.annotation.View;
-import com.khubla.pragmatach.framework.api.PragmatachException;
-import com.khubla.pragmatach.framework.api.Response;
-import com.khubla.pragmatach.plugin.freemarker.FreemarkerController;
-import com.xeiam.xchange.Exchange;
-import com.xeiam.xchange.ExchangeFactory;
-import com.xeiam.xchange.currency.Currencies;
-import com.xeiam.xchange.service.marketdata.polling.PollingMarketDataService;
+import com.khubla.pragmatach.framework.annotation.*;
+import com.khubla.pragmatach.framework.api.*;
+import com.khubla.pragmatach.plugin.freemarker.*;
+import com.xeiam.xchange.*;
+import com.xeiam.xchange.currency.*;
+import com.xeiam.xchange.service.polling.marketdata.*;
 
 /**
  * @author tome
@@ -17,28 +13,28 @@ import com.xeiam.xchange.service.marketdata.polling.PollingMarketDataService;
 @Controller(name = "IndexController")
 @View(view = "index.ftl")
 public class IndexController extends FreemarkerController {
-   /**
-    * the ticket
-    */
-   private String ticker;
+	/**
+	 * the ticket
+	 */
+	private String ticker;
 
-   public String getTicker() {
-      return ticker;
-   }
+	public String getTicker() {
+		return ticker;
+	}
 
-   @Route(uri = "/")
-   public Response render() throws PragmatachException {
-      try {
-         final Exchange mtGox = ExchangeFactory.INSTANCE.createExchange("com.xeiam.xchange.mtgox.v1.MtGoxExchange");
-         final PollingMarketDataService marketDataService = mtGox.getPollingMarketDataService();
-         ticker = marketDataService.getTicker(Currencies.BTC, Currencies.USD).toString();
-         return super.render();
-      } catch (final Exception e) {
-         throw new PragmatachException("Exception in render", e);
-      }
-   }
+	@Route(uri = "/")
+	public Response render() throws PragmatachException {
+		try {
+			final Exchange mtGox = ExchangeFactory.INSTANCE.createExchange("com.xeiam.xchange.mtgox.v1.MtGoxExchange");
+			final PollingMarketDataService marketDataService = mtGox.getPollingMarketDataService();
+			ticker = marketDataService.getTicker(CurrencyPair.BTC_USD).toString();
+			return super.render();
+		} catch (final Exception e) {
+			throw new PragmatachException("Exception in render", e);
+		}
+	}
 
-   public void setTicker(String ticker) {
-      this.ticker = ticker;
-   }
+	public void setTicker(String ticker) {
+		this.ticker = ticker;
+	}
 }
