e4ed6d41726daaf1ae2d0f1ce1cdd5ca6e9dc313
Tom Everett <tom@khubla.com>
2022-11-21 15:56:39 -0700

    fix

--- plugins/dbtestsuite/src/main/java/com/khubla/pragmatach/dbtestsuite/AbstractPersistenceTest.java
+++ plugins/dbtestsuite/src/main/java/com/khubla/pragmatach/dbtestsuite/AbstractPersistenceTest.java
@@ -1,90 +1,89 @@
 package com.khubla.pragmatach.dbtestsuite;
 
-import java.util.Set;
+import java.util.*;
 
-import org.testng.Assert;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
+import org.testng.*;
+import org.testng.annotations.*;
 
-import com.khubla.pragmatach.dbtestsuite.pojo.ExamplePOJO;
-import com.khubla.pragmatach.framework.api.Configuration;
-import com.khubla.pragmatach.framework.application.Application;
-import com.khubla.pragmatach.framework.dao.DAO;
-import com.khubla.pragmatach.framework.scanner.AnnotationScanner;
+import com.khubla.pragmatach.dbtestsuite.pojo.*;
+import com.khubla.pragmatach.framework.api.*;
+import com.khubla.pragmatach.framework.application.*;
+import com.khubla.pragmatach.framework.dao.*;
+import com.khubla.pragmatach.framework.scanner.*;
 
 /**
  * @author tome
  */
 public abstract class AbstractPersistenceTest {
-   /**
-    * get the configs
-    */
-   public abstract Set<Configuration> getConfigurations();
+	/**
+	 * get the configs
+	 */
+	public abstract Set<Configuration> getConfigurations();
 
-   /**
-    * get the DAO implementation
-    */
-   public abstract DAO<ExamplePOJO, Long> getDAO();
+	/**
+	 * get the DAO implementation
+	 */
+	public abstract DAO<ExamplePOJO, Long> getDAO();
 
-   /**
-    * for the class
-    */
-   @BeforeClass
-   public void setupClass() {
-      /*
-       * run the scanner
-       */
-      try {
-         AnnotationScanner.scan(null);
-      } catch (final Exception e) {
-         e.printStackTrace();
-         Assert.fail();
-      }
-   }
+	/**
+	 * for the class
+	 */
+	@BeforeClass
+	public void setupClass() {
+		/*
+		 * run the scanner
+		 */
+		try {
+			AnnotationScanner.scan();
+		} catch (final Exception e) {
+			e.printStackTrace();
+			Assert.fail();
+		}
+	}
 
-   @Test
-   public void testAllDatabaseConfigurations() {
-      try {
-         /*
-          * config
-          */
-         final Set<Configuration> configurations = getConfigurations();
-         if ((null != configurations) && (configurations.size() > 0)) {
-            /*
-             * set an initial config
-             */
-            Application.setConfiguration(configurations.iterator().next());
-            /*
-             * the dao
-             */
-            final DAO<ExamplePOJO, Long> dao = getDAO();
-            /*
-             * walk the config
-             */
-            for (final Configuration configuration : configurations) {
-               /*
-                * dump the config
-                */
-               for (String key : configuration.getAll().keySet()) {
-                  System.out.println(key + " : " + configuration.getParameter(key));
-               }
-               /*
-                * set the application config
-                */
-               Application.setConfiguration(configuration);
-               /*
-                * force a session factory reload
-                */
-               dao.reloadConfig();
-               /*
-                * run test
-                */
-               BasicTests.testBasicFunctionality(dao);
-            }
-         }
-      } catch (final Exception e) {
-         e.printStackTrace();
-         Assert.fail();
-      }
-   }
+	@Test
+	public void testAllDatabaseConfigurations() {
+		try {
+			/*
+			 * config
+			 */
+			final Set<Configuration> configurations = getConfigurations();
+			if ((null != configurations) && (configurations.size() > 0)) {
+				/*
+				 * set an initial config
+				 */
+				Application.setConfiguration(configurations.iterator().next());
+				/*
+				 * the dao
+				 */
+				final DAO<ExamplePOJO, Long> dao = getDAO();
+				/*
+				 * walk the config
+				 */
+				for (final Configuration configuration : configurations) {
+					/*
+					 * dump the config
+					 */
+					for (String key : configuration.getAll().keySet()) {
+						System.out.println(key + " : " + configuration.getParameter(key));
+					}
+					/*
+					 * set the application config
+					 */
+					Application.setConfiguration(configuration);
+					/*
+					 * force a session factory reload
+					 */
+					dao.reloadConfig();
+					/*
+					 * run test
+					 */
+					BasicTests.testBasicFunctionality(dao);
+				}
+			}
+		} catch (final Exception e) {
+			e.printStackTrace();
+			Assert.fail();
+		}
+	}
 }
