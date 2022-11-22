23728ac1612f1febca476d44f3cefe9e944222f0
Tom Everett <tom@khubla.com>
2022-11-21 19:29:50 -0700

    fix

--- pragmatach-framework/src/main/java/com/khubla/pragmatach/framework/crypto/AES.java
+++ pragmatach-framework/src/main/java/com/khubla/pragmatach/framework/crypto/AES.java
@@ -1,70 +1,68 @@
 package com.khubla.pragmatach.framework.crypto;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.security.MessageDigest;
-import java.util.Arrays;
+import java.io.*;
+import java.security.*;
+import java.util.*;
 
-import javax.crypto.Cipher;
-import javax.crypto.CipherInputStream;
-import javax.crypto.spec.SecretKeySpec;
+import javax.crypto.*;
+import javax.crypto.spec.*;
 
 import org.apache.commons.codec.binary.Base64;
-import org.apache.commons.io.IOUtils;
+import org.apache.commons.io.*;
 
 /**
  * @author tome
  */
 public class AES {
-   /**
-    * decrypt
-    */
-   public static String decrypt(String encryptedPayload, String key) throws Exception {
-      try {
-         final SecretKeySpec secretKeySpec = new SecretKeySpec(makeAESKey(key), "AES");
-         final Cipher cipher = Cipher.getInstance(KEYSPEC);
-         cipher.init(Cipher.DECRYPT_MODE, secretKeySpec);
-         final ByteArrayOutputStream baos = new ByteArrayOutputStream();
-         final CipherInputStream CipherInputStream = new CipherInputStream(new ByteArrayInputStream(Base64.decodeBase64(encryptedPayload.getBytes("UTF-8"))), cipher);
-         IOUtils.copy(CipherInputStream, baos);
-         return baos.toString("UTF-8");
-      } catch (final Exception e) {
-         throw new Exception("Exception in decrypt", e);
-      }
-   }
+	/**
+	 * decrypt
+	 */
+	public static String decrypt(String encryptedPayload, String key) throws Exception {
+		try {
+			final SecretKeySpec secretKeySpec = new SecretKeySpec(makeAESKey(key), "AES");
+			final Cipher cipher = Cipher.getInstance(KEYSPEC);
+			cipher.init(Cipher.DECRYPT_MODE, secretKeySpec);
+			final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			final CipherInputStream CipherInputStream = new CipherInputStream(new ByteArrayInputStream(Base64.decodeBase64(encryptedPayload.getBytes("UTF-8"))), cipher);
+			IOUtils.copy(CipherInputStream, baos);
+			return baos.toString("UTF-8");
+		} catch (final Exception e) {
+			throw new Exception("Exception in decrypt", e);
+		}
+	}
 
-   /**
-    * encrypt
-    */
-   public static String encrypt(String payload, String key) throws Exception {
-      try {
-         final SecretKeySpec secretKeySpec = new SecretKeySpec(makeAESKey(key), "AES");
-         final Cipher cipher = Cipher.getInstance(KEYSPEC);
-         cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec);
-         final CipherInputStream CipherInputStream = new CipherInputStream(new ByteArrayInputStream(payload.getBytes("UTF-8")), cipher);
-         final ByteArrayOutputStream baos = new ByteArrayOutputStream();
-         IOUtils.copy(CipherInputStream, baos);
-         return Base64.encodeBase64String(baos.toByteArray());
-      } catch (final Exception e) {
-         throw new Exception("Exception in encrypt", e);
-      }
-   }
+	/**
+	 * encrypt
+	 */
+	public static String encrypt(String payload, String key) throws Exception {
+		try {
+			final SecretKeySpec secretKeySpec = new SecretKeySpec(makeAESKey(key), "AES");
+			final Cipher cipher = Cipher.getInstance(KEYSPEC);
+			cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec);
+			final CipherInputStream CipherInputStream = new CipherInputStream(new ByteArrayInputStream(payload.getBytes("UTF-8")), cipher);
+			final ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			IOUtils.copy(CipherInputStream, baos);
+			return Base64.encodeBase64String(baos.toByteArray());
+		} catch (final Exception e) {
+			throw new Exception("Exception in encrypt", e);
+		}
+	}
 
-   /**
-    * make a nice AES key
-    */
-   private static byte[] makeAESKey(String key) throws Exception {
-      try {
-         final MessageDigest sha = MessageDigest.getInstance("SHA-1");
-         final byte[] rawkey = sha.digest(key.getBytes("UTF-8"));
-         return Arrays.copyOf(rawkey, 16); // use only first 128 bit
-      } catch (final Exception e) {
-         throw new Exception("Exception in makeAESKey", e);
-      }
-   }
+	/**
+	 * make a nice AES key
+	 */
+	private static byte[] makeAESKey(String key) throws Exception {
+		try {
+			final MessageDigest sha = MessageDigest.getInstance("SHA-1");
+			final byte[] rawkey = sha.digest(key.getBytes("UTF-8"));
+			return Arrays.copyOf(rawkey, 16); // use only first 128 bit
+		} catch (final Exception e) {
+			throw new Exception("Exception in makeAESKey", e);
+		}
+	}
 
-   /**
-    * keyspec
-    */
-   private final static String KEYSPEC = "AES/ECB/ISO10126Padding";
+	/**
+	 * keyspec
+	 */
+	private final static String KEYSPEC = "AES/CBC/ISO10126Padding";
 }
