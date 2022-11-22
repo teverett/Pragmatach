5f5f79742e3b378b3f432cce62bbb376559de07d
Tom Everett <tom@khubla.com>
2022-11-22 13:39:29 -0700

    resource leak fix

--- contrib/clientproxygenerator/src/main/java/com/khubla/pragmatach/contrib/clientproxygenerator/ClientGenerator.java
+++ contrib/clientproxygenerator/src/main/java/com/khubla/pragmatach/contrib/clientproxygenerator/ClientGenerator.java
@@ -1,90 +1,95 @@
 package com.khubla.pragmatach.contrib.clientproxygenerator;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.OutputStreamWriter;
-import java.util.List;
+import java.io.*;
+import java.util.*;
 
 /**
  * @author tome
  */
 public class ClientGenerator {
-   /**
-    * target dir
-    */
-   private final String targetDir;
+	/**
+	 * target dir
+	 */
+	private final String targetDir;
 
-   /**
-    * ctor
-    */
-   public ClientGenerator(String targetDir) {
-      this.targetDir = targetDir;
-   }
+	/**
+	 * ctor
+	 */
+	public ClientGenerator(String targetDir) {
+		this.targetDir = targetDir;
+	}
 
-   /**
-    * generate a file
-    */
-   public void generate(RouteUrl routeUrl, String namespace) throws Exception {
-      try {
-         /*
-          * name
-          */
-         String name = routeUrl.getParts().get(routeUrl.getParts().size() - 1);
-         /*
-          * stream
-          */
-         String path = System.getProperty("user.dir") + "/" + targetDir;
-         new File(path).mkdirs();
-         FileOutputStream fos = new FileOutputStream(path + "/" + name + ".java", false);
-         /*
-          * writer
-          */
-         OutputStreamWriter osw = new OutputStreamWriter(fos, "UTF-8");
-         /*
-          * package
-          */
-         osw.write("package " + namespace + ";\n");
-         /*
-          * HTTP Components
-          */
-         osw.write("import org.apache.http.client.methods.HttpGet;\n");
-         osw.write("import org.apache.http.impl.client.DefaultHttpClient;\n");
-         /*
-          * class
-          */
-         osw.write("public class " + name + " {\n");
-         /*
-          * method
-          */
-         osw.write("public void invoke() {\n");
-         osw.write("DefaultHttpClient client = new DefaultHttpClient();\n");
-         String url = generateUrl(routeUrl);
-         osw.write("}\n");
-         /*
-          * end of class
-          */
-         osw.write("}\n");
-         /*
-          * done
-          */
-         osw.close();
-         fos.flush();
-         fos.close();
-      } catch (final Exception e) {
-         throw new Exception("Exception in generate: '" + routeUrl.getContext() + "'", e);
-      }
-   }
+	/**
+	 * generate a file
+	 */
+	public void generate(RouteUrl routeUrl, String namespace) throws Exception {
+		OutputStreamWriter osw = null;
+		FileOutputStream fos = null;
+		try {
+			/*
+			 * name
+			 */
+			String name = routeUrl.getParts().get(routeUrl.getParts().size() - 1);
+			/*
+			 * stream
+			 */
+			String path = System.getProperty("user.dir") + "/" + targetDir;
+			new File(path).mkdirs();
+			fos = new FileOutputStream(path + "/" + name + ".java", false);
+			/*
+			 * writer
+			 */
+			osw = new OutputStreamWriter(fos, "UTF-8");
+			/*
+			 * package
+			 */
+			osw.write("package " + namespace + ";\n");
+			/*
+			 * HTTP Components
+			 */
+			osw.write("import org.apache.http.client.methods.HttpGet;\n");
+			osw.write("import org.apache.http.impl.client.DefaultHttpClient;\n");
+			/*
+			 * class
+			 */
+			osw.write("public class " + name + " {\n");
+			/*
+			 * method
+			 */
+			osw.write("public void invoke() {\n");
+			osw.write("DefaultHttpClient client = new DefaultHttpClient();\n");
+			String url = generateUrl(routeUrl);
+			osw.write("}\n");
+			/*
+			 * end of class
+			 */
+			osw.write("}\n");
+		} catch (final Exception e) {
+			throw new Exception("Exception in generate: '" + routeUrl.getContext() + "'", e);
+		} finally {
+			/*
+			 * done
+			 */
+			if (null != osw) {
+				osw.close();
+			}
+			if (null != fos) {
+				fos.flush();
+				fos.close();
+			}
+		}
+	}
 
-   public String getTargetDir() {
-      return targetDir;
-   }
+	public String getTargetDir() {
+		return targetDir;
+	}
 
-   private String generateUrl(RouteUrl routeUrl) {
-      String ret = "";
-      final List<String> parts = routeUrl.getParts();
-      for (final String part : parts) {
-         ret += "/" + part;
-      }
-      return ret;
-   }
+	private String generateUrl(RouteUrl routeUrl) {
+		String ret = "";
+		final List<String> parts = routeUrl.getParts();
+		for (final String part : parts) {
+			ret += "/" + part;
+		}
+		return ret;
+	}
 }
